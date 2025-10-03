06c06a69e4f424c52f81a4319c0c2e04b3761228
prikshitTech <prikshit.jaswal@auspicioussoft.in>
2025-10-03 11:17:41 +0530

    Url

--- src/utils/urls.tsx
+++ src/utils/urls.tsx
@@ -1,5 +1,5 @@
-const baseUrl = "http://localhost:8001/api/admin";
-// const baseUrl = "https://api.habibirizz.app/api/admin";
+// const baseUrl = "http://localhost:8001/api/admin";
+const baseUrl = "https://api.habibirizz.app/api/admin";
 
 export const URLS = {
   login: `${baseUrl}/login`,
